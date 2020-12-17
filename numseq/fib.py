a31fd4b48872d9b60d551ce9b8226e9dc07cfeed
akilmac <akilmac@hotmail.com>
2020-12-17 13:23:16 -0800

    initial commit

---
+++ numseq/fib.py
@@ -0,0 +1,11 @@
+def fib(n):
+    if n<=0:
+        print("Incorrect input")
+    # First Fibonacci number is 0
+    elif n==1:
+        return 0
+    # Second Fibonacci number is 1
+    elif n==2:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
